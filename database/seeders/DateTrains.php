57852b8a5143e0d65660687c8fb37340b89a0e61
LucaFaedda <lucakarl91@gmail.com>
2023-02-21 14:56:53 +0100

    provato a lanciare fake seed

--- database/seeders/DateTrains.php
+++ database/seeders/DateTrains.php
@@ -4,16 +4,33 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\train;
 
-class DateTrains extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(Faker $faker)
+
+    {   
+        for($i=0 ; $i<10; $i++){
+            $newTrain = new Train();
+            $newTrain->Azienda = $faker->text(50);
+            $newTrain->Stazione_partenza = $faker->text(50);
+            $newTrain->Stazione_arrivo = $faker->text(50);
+            $newTrain->Orario_partenza = $faker->time();
+            $newTrain->Orario_arrivo = $faker->time();
+            $newTrain->Codice_treno = $faker->numerify('treno-####');
+            $newTrain->Numero_carrozze = $faker->randomDigit();
+            $newTrain->In_orario = $faker->randomElement([true, false]);
+            $newTrain->Cancellatp = $faker->randomElement([true, false]);
+            $newTrain->save();
+
+        };
     }
 }
+
