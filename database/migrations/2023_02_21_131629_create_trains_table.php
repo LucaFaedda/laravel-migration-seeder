059d7c03a7ff77323f56903120dd5fef29031df7
LucaFaedda <lucakarl91@gmail.com>
2023-02-21 14:36:18 +0100

    collegato tabella completa

--- database/migrations/2023_02_21_131629_create_trains_table.php
+++ database/migrations/2023_02_21_131629_create_trains_table.php
@@ -16,6 +16,17 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('Azienda', 50);
+            $table->string('Stazione_partenza', 50);
+            $table->string('Stazione_arrivo', 50);
+            $table->time('Orario_partenza');
+            $table->time('Orario_arrivo');
+            $table->string('Codice_treno', 10);
+            $table->tinyInteger('Numero_carrozze');
+            $table->boolean('In_orario')->unsigned()->default(1);
+            $table->boolean('Cancellat0')->unsigned()->default(1);
+
+
         });
     }
 
